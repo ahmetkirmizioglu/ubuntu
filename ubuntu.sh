37408cbd782d62495ccedc7f1364c53704508d9f
Ahmet Kirmizioglu <ahmetkirmizioglu@gmail.com>
2023-10-21 13:03:21 +0300

    Update ubuntu.sh

--- ubuntu.sh
+++ ubuntu.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
-#Written and configured by Ahmet Kirmizioglu (kirmizioglu.net) for public use
-#1-Google public DNS / 8.8.8.8 and 8.8.4.4 will be added
-#2-Ubuntu GUI and XRDP will be installed for Ubuntu Server
-#3-Port 3386 will be opened
 
+echo "Written and configured by Ahmet Kirmizioglu (kirmizioglu.net) for public use"
+echo "1-Google public DNS / 8.8.8.8 and 8.8.4.4 will be added"
+echo "2-Ubuntu GUI and XRDP will be installed for Ubuntu Server"
+echo "3-Port 3386 will be opened"
+echo "---------------------------------------------------------------------------"
 echo "Did you read what was written above, okay?"
 echo "Press return to continue - Otherwise press CTRL+C to cancel the script"
 sleep 1
 read input1
 
-#Step 1
-#Creating a temporary folder..(ak)
+echo "Step 1"
+echo "Creating a temporary folder..(ak)"
 mkdir ak
 cd ak
 

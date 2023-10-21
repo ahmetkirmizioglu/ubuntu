36c3e4db4013dbf00c3207a7548718cbce7a5f9c
Ahmet Kirmizioglu <ahmetkirmizioglu@gmail.com>
2023-10-21 11:54:32 +0300

    Update ubuntu.sh

--- ubuntu.sh
+++ ubuntu.sh
@@ -4,5 +4,5 @@
 mkdir ak
 cd ak
 
-echo 'namerserver 8.8.8.8' >> /etc/resolv.conf
-echo 'namerserver 8.8.4.4' >> /etc/resolv.conf
+echo 'nameserver 8.8.8.8' >> /etc/resolv.conf
+echo 'nameserver 8.8.4.4' >> /etc/resolv.conf

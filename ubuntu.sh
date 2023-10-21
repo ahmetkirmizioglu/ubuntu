270bf06b61ff8e19fa0bf5fde486ce4a413f100b
Ahmet Kirmizioglu <ahmetkirmizioglu@gmail.com>
2023-10-21 13:16:05 +0300

    Update ubuntu.sh

--- ubuntu.sh
+++ ubuntu.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 
-red='\033[0;41m'
-green='\033[0;42m'
-yellow='\033[0;43m'
-blue='\033[0;44m'
-magenta='\033[0;45m'
-cyan='\033[0;46m'
+red='\033[0;31m'
+green='\033[0;32m'
+yellow='\033[0;33m'
+blue='\033[0;34m'
+magenta='\033[0;35m'
+cyan='\033[0;36m'
+clear='\033[0m'
 
 clear
-echo -e "${yellow}Written and configured by Ahmet Kirmizioglu (kirmizioglu.net) for public use${clear}!"
-echo -e "${yellow}1-Google public DNS / 8.8.8.8 and 8.8.4.4 will be added${clear}!"
-echo -e "${yellow}2-Ubuntu GUI and XRDP will be installed for Ubuntu Server${clear}!"
-echo -e "${yellow}3-Port 3386 will be opened${clear}!"
+echo -e "${green}Written and configured by Ahmet Kirmizioglu (kirmizioglu.net) for public use${clear}!"
+echo -e "${green}1-Google public DNS / 8.8.8.8 and 8.8.4.4 will be added${clear}!"
+echo -e "${green}2-Ubuntu GUI and XRDP will be installed for Ubuntu Server${clear}!"
+echo -e "${green}3-Port 3386 will be opened${clear}!"
 echo -e "---------------------------------------------------------------------------"
 echo -e "${magenta}Did you read what was written above, okay?${clear}!"
 echo -e "${magenta}Press return to continue - Otherwise press CTRL+C to cancel the script${clear}!"

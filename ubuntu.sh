6e8582ad4abd90c1d10bc4f66918ec3623da8f53
Ahmet Kirmizioglu <ahmetkirmizioglu@gmail.com>
2023-10-21 13:12:21 +0300

    Update ubuntu.sh

--- ubuntu.sh
+++ ubuntu.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
 
-RED='\033[0;31m'
-NC='\033[0m'
+red='\033[0;41m'
+green='\033[0;42m'
+yellow='\033[0;43m'
+blue='\033[0;44m'
+magenta='\033[0;45m'
+cyan='\033[0;46m'
 
 clear
-echo "${RED}Written and configured by Ahmet Kirmizioglu (kirmizioglu.net) for public use${NC}"
-echo "1-Google public DNS / 8.8.8.8 and 8.8.4.4 will be added"
-echo "2-Ubuntu GUI and XRDP will be installed for Ubuntu Server"
-echo "3-Port 3386 will be opened"
-echo "---------------------------------------------------------------------------"
-echo "Did you read what was written above, okay?"
-echo "Press return to continue - Otherwise press CTRL+C to cancel the script"
+echo -e "${yellow}Written and configured by Ahmet Kirmizioglu (kirmizioglu.net) for public use${clear}!"
+echo -e "${yellow}1-Google public DNS / 8.8.8.8 and 8.8.4.4 will be added${clear}!"
+echo -e "${yellow}2-Ubuntu GUI and XRDP will be installed for Ubuntu Server${clear}!"
+echo -e "${yellow}3-Port 3386 will be opened${clear}!"
+echo -e "---------------------------------------------------------------------------"
+echo -e "${magenta}Did you read what was written above, okay?${clear}!"
+echo -e "${magenta}Press return to continue - Otherwise press CTRL+C to cancel the script${clear}!"
 sleep 1
 read input1
 
